8e0996ab5667e6ff6603c675b15ad9208b1e9a69
Stefan <stefigabi89@yahoo.com>
2024-05-24 12:31:15 +0200

    Add project files.

---
+++ OnlineStore/Data/OnlineStoreDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace OnlineStore.Data
+{
+    public class OnlineStoreDbContext:DbContext
+    {
+        public OnlineStoreDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+    }
+}
