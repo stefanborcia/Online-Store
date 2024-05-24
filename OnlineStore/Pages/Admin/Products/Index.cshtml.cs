3604f49ac3d33fd69bf95ea6e20af6aba8b7b18e
Stefan <stefigabi89@yahoo.com>
2024-05-24 16:02:30 +0200

    Create Razor Page Products and add dropdown Menu

---
+++ OnlineStore/Pages/Admin/Products/Index.cshtml.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using OnlineStore.Data;
+using OnlineStore.Models;
+
+namespace OnlineStore.Pages.Admin.Products
+{
+    public class IndexModel : PageModel
+    {
+        private readonly OnlineStoreDbContext _dbContext;
+
+        public List<Product> Products {  get; set; } = new List<Product>();
+        public IndexModel(OnlineStoreDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public void OnGet()
+        {
+            Products=_dbContext.Products.OrderByDescending(p=>p.Id).ToList();
+        }
+    }
+}
