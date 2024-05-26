bc7c1b130a944df402a81c2d744814259a198cf6
Stefan <stefigabi89@yahoo.com>
2024-05-26 11:57:19 +0200

    Create Delete Razor Page and implement OnGet method

---
+++ OnlineStore/Pages/Admin/Products/Delete.cshtml.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using OnlineStore.Data;
+
+namespace OnlineStore.Pages.Admin.Products
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly IWebHostEnvironment _environment;
+        private readonly OnlineStoreDbContext _dbContext;
+
+        public DeleteModel(IWebHostEnvironment environment, OnlineStoreDbContext dbContext)
+        {
+            _environment=environment;
+            _dbContext=dbContext;
+        }
+        public void OnGet(int? id)
+        {
+            if (id == null)
+            {
+                Response.Redirect("/Admin/Products/Index");
+                return;
+            }
+
+            var product = _dbContext.Products.Find(id);
+            if (product == null)
+            {
+                Response.Redirect("/Admin/Products/Index");
+                return;
+            }
+
+            string imageFullPath = _environment.WebRootPath + "/product-images/" + product.ImageFileName;
+            System.IO.File.Delete(imageFullPath);
+
+            _dbContext.Products.Remove(product);
+            _dbContext.SaveChanges();
+
+            Response.Redirect("/Admin/Products/Index");
+        }
+    }
+}
