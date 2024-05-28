520cf4759442be4d525e438a3c19e25fb405384e
Stefan <stefigabi89@yahoo.com>
2024-05-28 13:47:03 +0200

    Update Register Razor Page to show a successful account created

--- OnlineStore/Pages/Account/Register.cshtml.cs
+++ OnlineStore/Pages/Account/Register.cshtml.cs
@@ -3,6 +3,9 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace OnlineStore.Pages.Account
 {
@@ -11,6 +14,7 @@ namespace OnlineStore.Pages.Account
         private readonly UserManager<IdentityUser> _userManager;
         private readonly RoleManager<IdentityRole> _roleManager;
         private readonly SignInManager<IdentityUser> _signInManager;
+
         public RegisterModel(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager, SignInManager<IdentityUser> signInManager)
         {
             _userManager = userManager;
@@ -20,50 +24,51 @@ namespace OnlineStore.Pages.Account
 
         [BindProperty]
         public InputModel Input { get; set; }
+
+        public List<SelectListItem> RoleItems { get; set; }
+
         public void OnGet()
         {
-            var roles = _roleManager.Roles.ToList();
-            var roleItems = new List<SelectListItem>();
-            foreach (var role in roles)
-            {
-                roleItems.Add(new SelectListItem { Text = role.Name, Value = role.Id });
-            }
-            ViewData["Roles"] = roleItems;
+            // Predefined roles (optional): Update with desired roles
+            RoleItems = new List<SelectListItem>
+      {
+        new SelectListItem { Value = "Manager", Text = "Manager" },
+        new SelectListItem { Value = "Customer", Text = "Customer" }
+      };
         }
-        public IActionResult OnPost()
+
+        public async Task<IActionResult> OnPostAsync()
         {
             if (ModelState.IsValid)
             {
                 var user = new IdentityUser { UserName = Input.Email, Email = Input.Email };
-                var result = _userManager.CreateAsync(user, Input.Password).Result;
+                var result = await _userManager.CreateAsync(user, Input.Password);
 
                 if (result.Succeeded)
                 {
-                    if (_userManager.IsInRoleAsync(user, Input.Role).Result)
+                    // Create the role if it doesn't exist
+                    var roleExist = await _roleManager.RoleExistsAsync(Input.Role);
+                    if (!roleExist)
                     {
-                        ModelState.AddModelError(string.Empty, "User already has the selected role.");
-                        return Page();
+                        await _roleManager.CreateAsync(new IdentityRole(Input.Role));
                     }
 
-                    var roleResult = _userManager.AddToRoleAsync(user, Input.Role).Result;
+                    var roleResult = await _userManager.AddToRoleAsync(user, Input.Role);
 
                     if (roleResult.Succeeded)
                     {
-                        return RedirectToPage("/Index");
+                        ViewData["registrationSuccessful"] = true;
                     }
-
-                    foreach (var error in roleResult.Errors)
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
                     {
                         ModelState.AddModelError(string.Empty, error.Description);
                     }
                 }
-
-                foreach (var error in result.Errors)
-                {
-                    ModelState.AddModelError(string.Empty, error.Description);
-                }
             }
-            // If we got this far, something failed, redisplay form
+
             return Page();
         }
 
@@ -82,6 +87,7 @@ namespace OnlineStore.Pages.Account
             [Display(Name = "Confirm password")]
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
             public string ConfirmPassword { get; set; }
+
             [Required]
             [Display(Name = "Role")]
             public string Role { get; set; }
